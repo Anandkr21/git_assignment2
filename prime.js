442b92c0f6f528aa0fe9c827ef3b429f75b4708d
Rahul Sharma <40212890+257277@users.noreply.github.com>
2022-09-20 16:53:18 +0530

    Member 2 is commiting correct code

--- prime.js
+++ prime.js
@@ -1,11 +1,16 @@
-let num=13;
 
-function check(num){
-    let count=0; 
+function check(num) {
+    let count = 0;
 
-    for(let i=1; i<=num; i++){
-        if(num%i==0){
-            return true;
+    for (let i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            count++;
         }
     }
+    if (count > 2) {
+        return false;
+    }
+    return true;
 }
+
+check(13);
